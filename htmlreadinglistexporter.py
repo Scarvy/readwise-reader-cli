76ee8c23ee24884369143adbe4bd9e29eb5a7061
Scott Carvalho <scottcarvalho71@gmail.com>
2023-08-12 12:15:22 -0400

    improve directory logic for saving and loading

--- htmlreadinglistexporter.py
+++ htmlreadinglistexporter.py
@@ -1,16 +1,21 @@
-import json
+from json import JSONEncoder, dumps, loads
 from collections import namedtuple
 from datetime import datetime
-from typing import List
+from typing import Any, List
 from pathlib import Path
 
+from xdg_base_dirs import xdg_data_home
 from bs4 import BeautifulSoup
 
-from document import DocumentInfo
-
 DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+CHROME_DEFAULT_FILENAME = "ReadingList"
 
 Bookmark = namedtuple("Bookmark", "title url add_date")
+
+
+class BookmarkEncoder(JSONEncoder):
+    def default(self, o: object) -> Any:
+        return str(o) if isinstance(o, Path) else o
 
 
 def utcfromtimestamp_in_microseconds(timestamp_microseconds: float) -> datetime:
@@ -23,14 +28,58 @@ def utcfromtimestamp_in_microseconds(timestamp_microseconds: float) -> datetime:
     return datetime_obj.strftime(DATETIME_FORMAT)
 
 
-def build_chrome_reading_list(file_path: str) -> List[Bookmark]:
-    if file_path.endswith(".html"):
-        with open(file_path, "r") as file:
-            html_content = file.read()
+def data_directory() -> Path:
+    (target_directory := xdg_data_home() / "reader" / "reading_lists").mkdir(
+        parents=True, exist_ok=True
+    )
+    return target_directory
+
+
+def reading_list_file() -> Path:
+    return data_directory() / "chrome.json"
+
+
+def save_reading_list(reading_list: list[Bookmark]) -> None:
+    reading_list_file().write_text(
+        dumps(reading_list, indent=4, sort_keys=True, cls=BookmarkEncoder)
+    )
+
+
+def load_reading_list(file_path: str = None) -> List[Bookmark]:
+    return (
+        [
+            Bookmark(title, Path(location), add_date)
+            for (title, location, add_date) in loads(reading_list.read_text())
+        ]
+        if (reading_list := reading_list_file()).exists()
+        else build_reading_list(file_path)
+    )
+
+
+def build_html_path(file_path: str = None) -> Path:
+    default_folder = Path.home() / "Downloads"
+
+    if file_path:
+        html_path = Path(file_path)
+        if not html_path.is_absolute():  # Check if provided file_path is a valid path
+            html_path = default_folder / html_path
+    else:
+        html_path = default_folder / CHROME_DEFAULT_FILENAME
+
+    html_path = html_path.with_suffix(".html")
+
+    return html_path
+
+
+def build_reading_list(file_path: str = None) -> None:
+    file_path = build_html_path(file_path)
+
+    with open(file_path, "r", encoding="utf-8") as f:
+        html_content = f.read()
 
     soup = BeautifulSoup(html_content, "html.parser")
 
-    chrome_reading_list = [
+    reading_list = [
         Bookmark(
             link.text,
             link.get("href"),
@@ -39,34 +88,18 @@ def build_chrome_reading_list(file_path: str) -> List[Bookmark]:
         for link in soup.find_all("a")
     ]
 
-    return chrome_reading_list
-
-
-def to_json(data: List[Bookmark]) -> None:
-    with open("chrome_reading_list.json", "w") as f:
-        json.dump(data, f, indent=4)
-
-
-def reading_list_to_json(file_path: str) -> None:
-    chrome_reading_list = build_chrome_reading_list(file_path=file_path)
-
-    to_json(chrome_reading_list)
-
-
-def get_reading_list() -> List[DocumentInfo]:
-    document_list = []
-
-    with open("chrome_reading_list.json", "r") as f:
-        chrome_reading_list = json.load(f)
-        for bookmark in chrome_reading_list:
-            url = bookmark[1]  # bookmark url
-            document_list.append(DocumentInfo(url, tags=["chrome-reading-list"]))
-    return document_list
+    save_reading_list(reading_list)
 
 
 if __name__ == "__main__":
-    file_path = "ReadingList.html"
+    # Example usage
+    file_path = "/path/to/CustomReadingList"
+    file_path = None
+    file_path = "CustomReadingList"
+    file_path = "/Users/scott_carvalho/projects/reader-import/ReadingList.html"
 
-    reading_list_to_json(file_path=file_path)
+    html_path = build_html_path(file_path)
+    print("HTML Path:", html_path)
 
-    chrome_reading_list = get_reading_list()
+    # load reading list
+    reading_list = load_reading_list(html_path)
