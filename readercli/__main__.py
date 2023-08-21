9424344c572de34b5162e66213269976ca4b03e9
Scott Carvalho <scottcarvalho71@gmail.com>
2023-08-21 19:36:31 -0400

    add --days option

--- readercli/__main__.py
+++ readercli/__main__.py
@@ -11,11 +11,19 @@ from .layout import table_layout
     "--location",
     "-l",
     type=str,
-    default=["archive"],
+    default="archive",
+    show_default=True,
     help="Document(s) location: `new`, `later`, `archive`, `feed`",
 )
-def cli(location):
-    full_data = fetch_documents(location=location)
+@click.option(
+    "--days",
+    "-d",
+    default=1,
+    show_default=True,
+    help="Updated after in days.",
+)
+def cli(location, days):
+    full_data = fetch_documents(updated_after=days, location=location)
     table_layout(full_data)
 
 
