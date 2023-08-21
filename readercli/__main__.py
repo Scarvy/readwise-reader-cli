fd1a4133e0f0798f13a8d97a280802aa008a3254
Scott Carvalho <scottcarvalho71@gmail.com>
2023-08-21 17:23:14 -0400

    working click rich table

--- readercli/__main__.py
+++ readercli/__main__.py
@@ -1,94 +1,23 @@
-"""Provides code to interact with the Reader API in the command-line"""
-import argparse
-
-from rich import print
-
-from .api import fetch_documents, add_document
-from .reading_list import load_reading_list
-from .constants import VALID_LOCATION_OPTIONS
-from .utils import count_category_values
-
-# Use a dictionary for location options and descriptions
-LOCATION_DESCRIPTIONS = {
-    "new": "New Documents",
-    "later": "Later Documents",
-    "archive": "Archived Documents",
-    "feed": "Feed Documents",
-}
+"""The main CLI module of readercli"""
+import click
 
 
-def main():
-    parser = argparse.ArgumentParser(
-        description="Save or List Documents from Readwise Reader",
-    )
-    subparsers = parser.add_subparsers(dest="command", required=False)
-
-    list_parser = subparsers.add_parser("list", help="List Documents")
-    list_parser.add_argument("-d", "--days", help="Updated after in days.")
-    list_parser.add_argument(
-        "-l",
-        "--location",
-        choices=VALID_LOCATION_OPTIONS,
-        help="Document location: `new`, `later`, `archive`, `feed`",
-    )
-
-    import_parser = subparsers.add_parser("import", help="Import Chrome Reading List")
-    import_parser.add_argument("-f", "--file", help="File path or name for HTML file")
-
-    add_parser = subparsers.add_parser("add", help="Add Document(s)")
-    add_parser.add_argument("-f", "--file", help="File path or name for HTML file")
-    add_parser.add_argument("-u", "--url", help="URL string for the document")
-
-    args = parser.parse_args()
-
-    if args.command == "list":
-        try:
-            if args.location or args.days:
-                if args.days:
-                    try:
-                        days = int(args.days)
-                    except ValueError as e:
-                        print("Error: ", e)
-                        days = None  # switch to None if user inputs a incorrect value that's not an int
-                else:
-                    days = args.days
-                full_data = fetch_documents(location=args.location, updated_after=days)
-            else:
-                full_data = fetch_documents()
-            print(full_data)
-            print("Number of documents:", len(full_data))
-            print("Category breakdown:", count_category_values(full_data))
-        except ValueError as e:
-            print("Error:", e)
-            if args.location not in VALID_LOCATION_OPTIONS:
-                print(f"'{args.location}' is not a valid location for Documents.")
-                print("Try:", ", ".join(LOCATION_DESCRIPTIONS.keys()))
-
-    elif args.command == "import":
-        print("Importing Reading List...")
-        reading_list = load_reading_list(args.file)
-        print(reading_list)
-
-    elif args.command == "add":
-        if args.file:
-            print(f"Adding Document(s) from file: {args.file}")
-            reading_list = load_reading_list(args.file)
-            documents_to_add = [{"url": document.url} for document in reading_list]
-            add_document_batch(documents_to_add)
-        elif args.url:
-            print(f"Adding Document from URL: {args.url}")
-            add_document(data={"url": args.url})
-        else:
-            print("No file or URL specified.")
-
-    else:
-        parser.print_help()
+from .api import fetch_documents
+from .layout import table_layout
 
 
-def add_document_batch(documents: list[dict]) -> None:
-    for document in documents:
-        add_document(data={"url": document.url})
+@click.command()
+@click.option(
+    "--location",
+    "-l",
+    type=str,
+    default=["archive"],
+    help="Document(s) location: `new`, `later`, `archive`, `feed`",
+)
+def cli(location):
+    full_data = fetch_documents(location=location)
+    table_layout(full_data)
 
 
 if __name__ == "__main__":
-    main()
+    cli()
---
+++ readercli/layout.py
@@ -0,0 +1,224 @@
+"""starcli.layouts"""
+
+from rich.console import Console
+
+from rich.table import Table
+from rich.text import Text
+
+console = Console()
+
+
+def table_layout(documents):
+    """Displays documents in a table format using rich"""
+
+    table = Table(leading=1)
+
+    # make the columns
+    table.add_column("Title", style="bold yellow")
+    table.add_column("Author")
+    table.add_column("Summary")
+    table.add_column("Location", justify="right")
+
+    for document in documents:
+        author = (
+            Text(document["author"], style="cyan")
+            if document["author"]
+            else Text("no author", style="italic")
+        )
+        summary = (
+            Text(document["summary"])
+            if document["summary"]
+            else Text("no summary", style="italic")
+        )
+
+        name = Text(document["title"], overflow="fold")
+        name.stylize(f"yellow link {document['url']}")
+
+        location = Text(document["location"], style="blue")
+
+        table.add_row(name, author, summary, location)
+
+    console.print(table)
+
+
+if __name__ == "__main__":
+    documents = [
+        {
+            "id": "01h8707jqgjsfzznh281xxashb",
+            "url": "https://read.readwise.io/read/01h8707jqgjsfzznh281xxashb",
+            "title": "Large language models, explained with a minimum of math and jargon",
+            "author": "Timothy B Lee",
+            "source": None,
+            "category": "article",
+            "location": "new",
+            "tags": {
+                "shortlist": {
+                    "name": "shortlist",
+                    "type": "manual",
+                    "created": 1692640034398,
+                }
+            },
+            "site_name": "understandingai.org",
+            "word_count": 6117,
+            "created_at": "2023-08-19T13:37:24.567027+00:00",
+            "updated_at": "2023-08-21T18:34:24.731100+00:00",
+            "published_date": 1690416000000,
+            "summary": "Want to really understand how large language models work? Here’s a gentle primer.",
+            "image_url": "https://substackcdn.com/image/fetch/w_1200,h_600,c_fill,f_jpg,q_auto:good,fl_progressive:steep,g_auto/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2Fc3e159bd-1228-4205-b1eb-5898ab9172d3_1600x856.png",
+            "content": None,
+            "source_url": "https://link.sbstck.com/redirect/08f86d22-19a6-4cc1-b449-0eebe03603dd?j=eyJ1IjoiY2gzcTgifQ.mfKJkm1MFmHg0LGeKdRGUAyRwWsVKwDUCVREGN0XyUw",
+            "notes": "A model so vast,\nWords flow like a river wide,\nIntelligence reigns.",
+            "parent_id": None,
+            "reading_progress": 0.035575772085228635,
+        },
+        {
+            "id": "01h7jnwdem0v454jfz6jt3999z",
+            "url": "https://read.readwise.io/read/01h7jnwdem0v454jfz6jt3999z",
+            "title": "Python Design Patterns¶",
+            "author": "python-patterns.guide",
+            "source": "Readwise web highlighter",
+            "category": "article",
+            "location": "new",
+            "tags": {
+                "python": {
+                    "name": "Python",
+                    "type": "manual",
+                    "created": 1691770320268,
+                },
+                "resources": {
+                    "name": "resources",
+                    "type": "manual",
+                    "created": 1691770341678,
+                },
+                "shortlist": {
+                    "name": "shortlist",
+                    "type": "manual",
+                    "created": 1691771937889,
+                },
+                "programming": {
+                    "name": "programming",
+                    "type": "manual",
+                    "created": 1691770334734,
+                },
+                "design-patterns": {
+                    "name": "design-patterns",
+                    "type": "manual",
+                    "created": 1691770329111,
+                },
+            },
+            "site_name": "python-patterns.guide",
+            "word_count": 118,
+            "created_at": "2023-08-11T16:11:45.191410+00:00",
+            "updated_at": "2023-08-16T23:29:30.819847+00:00",
+            "published_date": None,
+            "summary": "I’m Brandon Rhodes (website, Twitter) and this is my evolving guide to design patterns in the Python programming language.",
+            "image_url": "",
+            "content": None,
+            "source_url": "https://python-patterns.guide/",
+            "notes": "",
+            "parent_id": None,
+            "reading_progress": 1,
+        },
+        {
+            "id": "01h7gwmcthew5qn35wvdkpv8aj",
+            "url": "https://read.readwise.io/read/01h7gwmcthew5qn35wvdkpv8aj",
+            "title": "Python Command-Line Arguments",
+            "author": "Real Python",
+            "source": "Readwise web highlighter",
+            "category": "article",
+            "location": "new",
+            "tags": {
+                "shortlist": {
+                    "name": "shortlist",
+                    "type": "manual",
+                    "created": 1691725386688,
+                }
+            },
+            "site_name": "realpython.com",
+            "word_count": 12352,
+            "created_at": "2023-08-10T23:31:12.964479+00:00",
+            "updated_at": "2023-08-21T17:48:55.708477+00:00",
+            "published_date": 1580860800000,
+            "summary": "Python command-line arguments are the key to converting your programs into useful and enticing tools that are ready to be used in the terminal of your operating system. In this step-by-step tutorial, you'll learn their origins, standards, and basics, and how to implement them in your program.",
+            "image_url": "https://files.realpython.com/media/Python-Command-Line-Arguments_Watermarked.33cee612a4ae.jpg",
+            "content": None,
+            "source_url": "https://realpython.com/python-command-line-arguments/",
+            "notes": "",
+            "parent_id": None,
+            "reading_progress": 0.9019759274910384,
+        },
+        {
+            "id": "01h65f74nbcywcc37v29rkymrj",
+            "url": "https://read.readwise.io/read/01h65f74nbcywcc37v29rkymrj",
+            "title": "RESTful Web Services",
+            "author": "Leonard Richardson",
+            "source": "File Upload",
+            "category": "pdf",
+            "location": "new",
+            "tags": {
+                "url": {"name": "URL", "type": "manual", "created": 1690397218500},
+                "xml": {"name": "XML", "type": "manual", "created": 1690397222773},
+                "http": {"name": "HTTP", "type": "manual", "created": 1690397212606},
+                "reading": {
+                    "name": "reading",
+                    "type": "manual",
+                    "created": 1691725499900,
+                },
+                "shortlist": {
+                    "name": "shortlist",
+                    "type": "manual",
+                    "created": 1691725489429,
+                },
+                "restful-api": {
+                    "name": "RESTful-API",
+                    "type": "manual",
+                    "created": 1690397205219,
+                },
+            },
+            "site_name": "readwise.io",
+            "word_count": 152889,
+            "created_at": "2023-07-25T02:49:27.806466+00:00",
+            "updated_at": "2023-08-21T18:34:54.375418+00:00",
+            "published_date": 1297900800000,
+            "summary": "",
+            "image_url": "",
+            "content": None,
+            "source_url": "https://readwise.io/reader/document_raw_content/68888514",
+            "notes": "",
+            "parent_id": None,
+            "reading_progress": 0.033482142857142856,
+        },
+        {
+            "id": "01h65f6zw9y1b0kx4png80mmv1",
+            "url": "https://read.readwise.io/read/01h65f6zw9y1b0kx4png80mmv1",
+            "title": "Web API Design",
+            "author": "Helen Whelan",
+            "source": "File Upload",
+            "category": "pdf",
+            "location": "new",
+            "tags": {
+                "api": {"name": "API", "type": "manual", "created": 1690397106792},
+                "web": {"name": "Web", "type": "manual", "created": 1690397114367},
+                "ebook": {"name": "ebook", "type": "manual", "created": 1690397149271},
+                "shortlist": {
+                    "name": "shortlist",
+                    "type": "manual",
+                    "created": 1691725432090,
+                },
+            },
+            "site_name": "readwise.io",
+            "word_count": 7661,
+            "created_at": "2023-07-25T02:49:21.239292+00:00",
+            "updated_at": "2023-08-21T18:34:50.675757+00:00",
+            "published_date": 1332288000000,
+            "summary": "",
+            "image_url": "",
+            "content": None,
+            "source_url": "https://readwise.io/reader/document_raw_content/74070770",
+            "notes": "",
+            "parent_id": None,
+            "reading_progress": 0.6842105263157895,
+        },
+    ]
+
+    table_layout(documents=documents)
