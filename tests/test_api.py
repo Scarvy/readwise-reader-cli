6fed2ee794fb6872d64bd28036a910e01b028615
Scott Carvalho <scottcarvalho71@gmail.com>
2023-09-06 20:36:58 -0400

    Add `pytest` #7 clean up

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,23 +1,9 @@
 import pytest
-from readercli.api import (
-    APIHandler,
-)
+
+from readercli.api import validate_token
 
 
-@pytest.fixture
-def api_handler():
-    return APIHandler()
-
-
-def test_validate_token_invalid(api_handler):
+def test_validate_token_invalid():
     token = "invalid_token"
-    result = api_handler.validate_token(token)
+    result = validate_token(token)
     assert result is False
-
-
-def test_fetch_documents(api_handler):
-    pass
-
-
-def test_add_document(api_handler):
-    pass
