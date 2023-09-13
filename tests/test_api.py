4c1d264796e000983acb139181880857314b0859
Scott Carvalho <scottcarvalho71@gmail.com>
2023-09-13 11:48:47 -0400

    ♻️ Refactor API tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -7,65 +7,66 @@ from readercli.api import validate_token, _get_list, _create_doc
 # source https://stackoverflow.com/questions/15753390/how-can-i-mock-requests-and-the-response
 def mock_request_get(**kwargs):
     class MockGetResponse:
-        def __init__(self, count, next_page_cursor, json_data):
-            self.count = count
-            self.next_page_cursor = next_page_cursor
-            self.results = [json_data]
+        def __init__(self, status_code, count, next_page_cursor, results):
+            self.status_code = status_code
+            self.json = [
+                {"count": count, "nextPageCursor": next_page_cursor, "results": results}
+            ]
 
-        def count(self):
-            return self.count
-
-        def next_page_cursor(self):
-            return self.next_page_cursor
+        def status_code(self):
+            return self.status_code
 
         def resutls(self):
             return self.results
 
-    if kwargs["params"]["location"] == "new":
-        return MockGetResponse(2304, "01gm6kjzabcd609yepjrmcgz8a", {"location": "new"})
-    if kwargs["params"]["location"] == "later":
+    location = kwargs.get("location")
+
+    if location:
         return MockGetResponse(
-            2304, "01gm6kjzabcd609yepjrmcgz8a", {"location": "later"}
+            200, 2304, "01gm6kjzabcd609yepjrmcgz8a", {"key1": "value1"}
         )
 
-    return MockGetResponse(None, None, None)
+    return MockGetResponse(200, 0, None, None)
 
 
 @mock.patch("requests.get", side_effect=mock_request_get, autospec=True)
-def test_get_list_new_location(_):
-    _get_list({"location": "new"})
+def test_get_list(_):
+    _get_list(params={"location": "new"})
 
 
 @mock.patch("requests.get", side_effect=mock_request_get, autospec=True)
-def test_get_list_later_location(_):
-    _get_list({"location": "later"})
+def test_get_list_empty(_):
+    _get_list(params={"location": None})
 
 
 def mock_request_post(**kwargs):
     class MockPostResponse:
-        def __init__(self, _id, url):
-            self.id = _id
-            self.url = url
+        def __init__(self, json_data, status_code):
+            self.json = json_data
+            self.status_code = status_code
 
-        def _id(self):
-            return self.id
+        def json(self):
+            return self.json
 
-        def url(self):
-            return self.url
-
-    if kwargs["json"]["url"] == "https://new_article.com":
-        return MockPostResponse(
-            "0000ffff2222eeee3333dddd4444",
-            "https://read.readwise.io/new/read/0000ffff2222eeee3333dddd4444",
-        )
+        def status_code(self):
+            return self.status_code
 
     if kwargs["json"]["url"] == "https://new_article.com":
         return MockPostResponse(
-            "0000ffff2222eeee3333dddd4444",
-            "https://read.readwise.io/new/read/0000ffff2222eeee3333dddd4444",
+            {"key1", "value1"},
+            201,
         )
 
-    return MockPostResponse(None, 404)
+    if kwargs["json"]["url"] == "https://already_exist_article.com":
+        return MockPostResponse(
+            {"key1", "value1"},
+            200,
+        )
+
+    if kwargs["json"]["url"] == "bad_article_url":
+        return MockPostResponse({"key1": "value1"}, 400)
+
+    return MockPostResponse(None, 400)
 
 
 @mock.patch("requests.post", side_effect=mock_request_post, autospec=True)
@@ -76,6 +77,11 @@ def test_create_new_document(_):
 @mock.patch("requests.post", side_effect=mock_request_post, autospec=True)
 def test_create_already_exist_document(_):
     _create_doc(info={"url": "https://already_exist_article.com"})
+
+
+@mock.patch("requests.post", side_effect=mock_request_post, autospec=True)
+def test_create_invalid_url_document(_):
+    _create_doc(info={"url": "bad_article_url"})
 
 
 def test_validate_token_invalid():
